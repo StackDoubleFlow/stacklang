71cb75514ddcaf1d5e5e641123b25ca2afea9077
StackDoubleFlow <ojaslandge@gmail.com>
2019-11-18 16:14:53 -0600

    Better error messages

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ mod syntax;
 use syntax::lexer::Lexer;
 
 fn main() {
-    let mut lexer = Lexer::new("'fish' + 'fish' + foo + 10121");
-    lexer.lex().unwrap();
-    println!("Tokens: {:#?}", lexer.tokens);
+    let mut lexer = Lexer::new("'fish' + 'fish' + foo + 101868762148313247621");
+    match lexer.lex() {
+        Err(e) => {
+            eprintln!("{}:{} :: {}", lexer.line, lexer.col, e);
+        }
+        Ok(_) => {
+            println!("Tokens: {:#?}", lexer.tokens);
+        }
+    };
 }
